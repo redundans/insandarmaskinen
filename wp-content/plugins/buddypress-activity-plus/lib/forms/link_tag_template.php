f76f3c2814757ff7d7e1b6be5bb720a200ccd8f2
Jesper <jesper@klandestino.se>
2012-10-14 11:23:19 +0200

    Stilar upp meddelanden

--- wp-content/plugins/buddypress-activity-plus/lib/forms/link_tag_template.php
+++ wp-content/plugins/buddypress-activity-plus/lib/forms/link_tag_template.php
@@ -1,10 +1,10 @@
 <div class="bpfb_final_link row">
 	<?php if ($image) { ?>
-	<div class="bpfb_link_preview_container span2">
+	<div class="span2">
 		<a href="<?php echo $url;?>"><img src="<?php echo $image; ?>" /></a>
 	</div>
 	<?php } ?>
-	<div class="bpfb_link_contents span4">
+	<div class="span4">
 		<div class="bpfb_link_preview_title"><?php echo $title;?></div>
 		<div class="bpfb_link_preview_url">
 			<a href="<?php echo $url;?>"><?php echo $url;?></a>
